050705d54d851a0566a4f7b6cd48b79d6d5705d0
DiegoJfsr <diegojfsr@gmail.com>
2024-12-21 21:22:31 -0300

    AplicacaoReactRouter

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        {/* <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav> */}
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+        <div>
+          <Link to="/">
+            <button>Home</button>
+          </Link>
+          <Link to="/about">
+            <button>About</button>
+          </Link>
+          <Link to="/contact">
+            <button>Contact</button>
+          </Link>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
